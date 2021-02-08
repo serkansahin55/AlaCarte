0bd10b0e71d936c3feee79d7735cefae79975e6b
serkansahin55 <ssahin55@outlook.com>
2021-02-07 20:15:32 -0500

    implemented API backend calls for reviews and favorites creation

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -3,4 +3,20 @@ class FavoritesController < ApplicationController
         @favorites = Favorite.all
         render json: @favorites, status: :ok
     end
+
+    def create
+        @favorite = Favorite.new(favorite_params)
+        
+        if @favorite.save
+            render json: @favorite, status: :ok
+        else
+            render json: @favorite.errors.messages, status: 422
+        end
+    end
+
+    private
+
+    def favorite_params
+        params.require(:favorite).permit(:user_id, :menu_item_id)
+    end
 end
--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -3,4 +3,20 @@ class ReviewsController < ApplicationController
         @reviews = Review.all
         render json: @reviews, status: :ok
     end
+
+    def create
+        @review = Review.new(review_params)
+        
+        if @review.save
+            render json: @review, status: :ok
+        else
+            render json: @review.errors.messages, status: 422
+        end
+    end
+
+    private
+
+    def review_params
+        params.require(:review).permit(:text, :rating, :user_id, :restaurant_id)
+    end
 end
