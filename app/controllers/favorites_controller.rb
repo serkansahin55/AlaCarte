fab89cb0eb132db6dd59da4faa9d5932e53850dd
serkansahin55 <ssahin55@outlook.com>
2021-02-07 19:39:55 -0500

    added controllers

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -1,2 +1,6 @@
 class FavoritesController < ApplicationController
+    def index
+        @favorites = Favorite.all
+        render json: @favorites, status: :ok
+    end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,4 @@
 class User < ApplicationRecord
-    # belongs_to :restaurant
     has_many :reviews
     has_many :favorites
 end
